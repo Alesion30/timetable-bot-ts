7d94470517126f3103d8e12052d396a89558d7d0
Yuta Ohira <alesion90@gmail.com>
2022-07-16 21:06:38 +0900

    add express server

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('hello, world');
+import express, { Express } from 'express';
+
+const midleware = (app: Express) => {
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  return app;
+};
+
+const app = midleware(express());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log('Running at http://localhost:3000');
+});
