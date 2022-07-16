2b218cf57d9679e4f9e2119a4b1db303f3e1c5b0
Yuta Ohira <alesion90@gmail.com>
2022-07-16 22:04:48 +0900

    add LINE_CHANNEL_ACCESS_TOKEN

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,3 +2,6 @@ import 'dotenv/config';
 
 /** ポート番号 */
 export const PORT = process.env.PORT || 3000;
+
+/** LINE アクセストークン */
+export const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
